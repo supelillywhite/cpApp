932f8a81e264663373443632f3abd631c0367b1e
Jerel Lillywhite <supelillywhite@Jerels-MBP.ad.uvu.edu>
2017-12-15 13:31:41 -0700

    Created pages and update home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/faq'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
